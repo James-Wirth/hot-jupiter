4ecae3ca60f0a14c4917b0d05fe469479c57d7cd
James Wirth <jameswirth@mac.home>
2025-04-18 16:08:31 +0100

    add results processor class

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-from hjmodel import HJModel
 import os
 import matplotlib.pyplot as plt
-from hjmodel.fixed_cluster import Plummer
-import scienceplots
 
-import dask.dataframe as dd
-
-from pathlib import Path
 plt.style.use(['science','nature'])
 
-def main():
+def get_res_path(exp_name: str):
     dir_path = os.path.dirname(os.path.realpath(__file__))
+    res_path = os.path.join(dir_path, 'data', f'exp_data_{exp_name}.pq')
+    return res_path
+
+def run(exp_name: str):
+    from hjmodel import HJModel
+    from clusters.plummer import Plummer
 
     for i in range(0, 1):
-        cluster_name = f'47_TUC_HYBRID_RUN{i}'
-        res_path = os.path.join(dir_path, 'data', f'exp_data_{cluster_name}.pq')
+        run_name = f'{exp_name}_RUN{i}'
+        res_path = get_res_path(exp_name=run_name)
 
         plummer = Plummer()
         model = HJModel(res_path=res_path)
         model.run_dynamic(time=12000, num_systems=50000, cluster=plummer, hybrid_switch=True)
 
+def plot(exp_name: str):
+    from hjmodel import HJModel
+    from hjmodel.processor import Processor
+
+    res_path = get_res_path(exp_name=exp_name)
+    model = HJModel(res_path=res_path)
+
+    # example to show probability vs projected radius
+    fig, ax = plt.subplots()
+    processor = Processor(data=model.df)
+    processor.plot_projected_probability(ax=ax)
+    plt.show()
+
+
 
 if __name__ == '__main__':
-    main()
+    plot(exp_name="47_TUC_HYBRID")
 
 
